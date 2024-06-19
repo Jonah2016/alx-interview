2d8a0960f7ffb8f37fa13314652e97aac253d78b
“Jonah <etuafuljonah@gmail.com>
2024-06-19 13:19:27 +0000

    full corrected code

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
-""" Module for solving prime game question """
+"""Prime game"""
+
+
+def era_sieve(primes):
+    """Use Erasthonenes sieve to find primes"""
+    outter_idx = 0
+    while outter_idx < len(primes):
+        innner_idx = outter_idx + 1
+        while innner_idx < len(primes):
+            if primes[innner_idx] % primes[outter_idx] == 0:
+                primes.pop(innner_idx)
+            else:
+                innner_idx += 1
+            outter_idx += 1
+    return primes
+
 
 def isWinner(x, nums):
-    """function that checks for the winner"""
-    if not nums or x < 1:
-        return None
-    max_num = max(nums)
-
-    my_filter = [True for _ in range(max(max_num + 1, 2))]
-    for i in range(2, int(pow(max_num, 0.5)) + 1):
-        if not my_filter[i]:
-            continue
-        for j in range(i * i, max_num + 1, i):
-            my_filter[j] = False
-    my_filter[0] = my_filter[1] = False
-    y = 0
-    for i in range(len(my_filter)):
-        if my_filter[i]:
-            y += 1
-        my_filter[i] = y
-    player1 = 0
-    for x in nums:
-        player1 += my_filter[x] % 2 == 1
-    if player1 * 2 == len(nums):
-        return None
-    if player1 * 2 > len(nums):
-        return "Maria"
-    return "Ben"
+    """Get the game winner"""
+    maria_score = 0
+    ben_score = 0
+    for game in range(x):
+        primes_list = era_sieve([i for i in range(2, nums[game])])
+        if len(primes_list) % 2 == 0:
+            ben_score += 1
+        else:
+            maria_score += 1
+    if maria_score > ben_score:
+        return 'Maria'
+    if ben_score > maria_score:
+        return 'Ben'
+    return None
